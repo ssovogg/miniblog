f642ff00cf904b5daa3c25ec7fabb2d604b98fd3
ssovogg <susuco77@gmail.com>
2022-09-20 17:28:09 +0900

    add edit mode

---
+++ src/components/EditForm/EditForm.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useRef } from "react";
+import classes from "./EditForm.module.css";
+
+const EditForm = ({ onCancle, item }) => {
+  const fileRef = useRef();
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  const fileChange = () => {
+    fileRef.current.click();
+  };
+  return (
+    <>
+    <form className={classes.edit_form} onSubmit={onSubmit}>
+      <input type="date" value={item.date}/>
+      <div className={classes.img}>
+        <button className={classes.fileBtn} onClick={fileChange}>
+          사진 변경
+        </button>
+        <img src={item.image} alt={item.title} />
+      </div>
+      <input type="file" id="pic" ref={fileRef} />
+      <div>
+        <input type="text" id="title" placeholder="제목" value={item.title}/>
+        <textarea id="content" placeholder="본문" value={item.text}/>
+        <button type="submit" className={classes.submitBtn}>
+          게시
+        </button>
+        <button onClick={onCancle} className={classes.cancleBtn}>
+          취소
+        </button>
+      </div>
+    </form>
+    </>
+  );
+};
+
+export default EditForm;
--- src/components/ShowList/ShowList.jsx
+++ src/components/ShowList/ShowList.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRef } from "react";
+import EditForm from "../EditForm/EditForm";
 import classes from "./ShowList.module.css";
 
 const Back = (props) => (
@@ -7,36 +8,44 @@ const Back = (props) => (
 );
 
 const ShowList = ({ onCancle, item, user }) => {
-  const fileRef = useRef();
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const [contents, setContents] = useState(item);
+  const [editMode, setEditMode] = useState(false);
+  const toggleEditMode = () => {
+    setEditMode((prev) => !prev);
+    console.log(contents);
   };
-  const fileChange = () => {
-    fileRef.current.click();
-  };
+
   return (
     <>
-      <form className={classes.edit_form} onSubmit={onSubmit}>
-        <input type="date" value={item.date} readOnly/>
-        <div className={classes.img}>
-          <img src={item.image} alt="" />
-        </div>
-        <div className={classes.content}>
-          <h2>{item.title}</h2>
-          <pre>{item.text}</pre>
-          {user && (
-            <>
-              <button type="submit" className={classes.submitBtn}>
-                수정
-              </button>
-              <button className={classes.deleteBtn}>삭제</button>
-            </>
-          )}
-          <button onClick={onCancle} className={classes.cancleBtn}>
-            X
-          </button>
-        </div>
-      </form>
+      {editMode ? (
+        <EditForm item={contents} />
+      ) : (
+        <form className={classes.edit_form}>
+          <input type="date" value={item.date} readOnly />
+          <div className={classes.img}>
+            <img src={item.image} alt="" />
+          </div>
+          <div className={classes.content}>
+            <h2>{item.title}</h2>
+            <pre>{item.text}</pre>
+            {user && (
+              <>
+                <button
+                  type="submit"
+                  className={classes.submitBtn}
+                  onClick={toggleEditMode}
+                >
+                  수정
+                </button>
+                <button className={classes.deleteBtn}>삭제</button>
+              </>
+            )}
+            <button onClick={onCancle} className={classes.cancleBtn}>
+              X
+            </button>
+          </div>
+        </form>
+      )}
       <Back onClick={onCancle} />
     </>
   );
